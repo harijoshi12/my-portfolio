4ea944358c0c571ac7a7a2d29cc94718c1083a8f
harijoshi12 <mr.hariprasadjoshi@gmail.com>
2022-06-24 20:25:21 +0530

    resumepage data page complete

--- src/pages/myResume/data.js
+++ src/pages/myResume/data.js
@@ -1,3 +1,37 @@
+
+export const contactDetails =[
+    {
+        id: 1,
+        icon: "Email:",
+        info: "mr.hariprasdjoshi@gmail.com",
+        link: "mailto:mr.hariprasdjoshi@gmail.com"
+    },
+    {
+        id: 2,
+        icon: "Phone:",
+        info: "9634090498",
+        link: "tel:+9634090498"
+    },
+    {
+        id: 3,
+        icon: "Location:",
+        info: "Dehradun, India",
+        link: "https://goo.gl/maps/pvYsfgd7Kv9RYVut9"
+    },
+    {
+        id: 4,
+        icon: "Linkedin:",
+        info: "linkedin.com/in/harijoshi123",
+        link: "https://www.linkedin.com/in/harijoshi123/"
+    },
+    {
+        id: 5,
+        icon: "My Website:",
+        info: "harijoshi.info",
+        link: "https://www.harijoshi.info/"
+    }
+]
+
 export const projectData = [
     {
         id: 1,
@@ -54,3 +88,179 @@ export const myJourneyData = [
         desc: "i learned coding from 0 to intermediate & advance level by watching tutorials, reading books & taking some paid courses"
     }
 ]
+
+export const interests = [
+    {
+        id:1,
+        title: "Coding"
+    },
+    {
+        id:2,
+        title: "Solving Puzzles"
+    },
+    {
+        id:3,
+        title: "Reading Science & Technology related Articles"
+    },
+    {
+        id:4,
+        title: "Watching Ted Talks & Documentries"
+    },
+    {
+        id:5,
+        title: "Listning Audio Books"
+    },
+    {
+        id:6,
+        title: "Exercising"
+    },
+    {
+        id:7,
+        title: "Learning New Skills"
+    },
+]
+
+export const techSkills = [
+    {
+        id: 1,
+        title: "React"
+    },
+    {
+        id: 2,
+        title: "React Native"
+    },
+    {
+        id: 3,
+        title: "Next.js"
+    },
+    {
+        id: 4,
+        title: "SASS"
+    },
+    {
+        id: 5,
+        title: "Bootstrap"
+    },
+    {
+        id: 6,
+        title: "TailWind"
+    },
+    {
+        id: 7,
+        title: "Git & Github"
+    },
+    {
+        id: 8,
+        title: "Node.js"
+    },
+    {
+        id: 9,
+        title: "Mongodb"
+    },
+    {
+        id: 10,
+        title: "Express.js"
+    },
+    {
+        id: 11,
+        title: "Django"
+    },
+    {
+        id: 12,
+        title: "PostgreSQL"
+    },
+    {
+        id: 13,
+        title: "DSA"
+    },
+    {
+        id: 14,
+        title: "Blockchain Development"
+    },
+    {
+        id: 15,
+        title: "Wordpress Theme & Plugin Development"
+    },
+    {
+        id: 16,
+        title: "Linux"
+    },
+    {
+        id: 17,
+        title: "Computer Networking"
+    },
+    {
+        id: 18,
+        title: "AWS"
+    },
+    {
+        id: 19,
+        title:"Cyber Security"
+    },
+    {
+        id: 20,
+        title:"Unity"
+    },
+    {
+        id: 21,
+        title:"Blender"
+    },
+    {
+        id: 22,
+        title:"IOT"
+    }
+]
+
+export const progLangs = [
+    {
+       id: 1,
+       title: "Html/Css",
+       level:  9
+    },
+    {
+       id: 2,
+       title: "JavaScript",
+       level:  9
+    },
+    {
+       id: 3,
+       title: "Python",
+       level:  8
+    },
+    {
+       id: 4,
+       title: "SQL",
+       level:  8
+    },
+    {
+       id: 5,
+       title: "C++",
+       level:  7
+    },
+    {
+       id: 6,
+       title: "Java",
+       level:  7
+    },
+    {
+       id: 7,
+       title: "GoLang",
+       level:  6
+    },
+    {
+       id: 8,
+       title: "Solidity",
+       level:  6
+    },
+    {
+       id: 9,
+       title: "Php",
+       level:  6
+    },
+    {
+       id: 10,
+       title: "TypeScript",
+       level:  6
+    }
+]
+
