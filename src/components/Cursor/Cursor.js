31c0fe7ea28fec182c3dbca8edb30965a75d6592
harijoshi12 <mr.hariprasadjoshi@gmail.com>
2022-07-11 21:17:54 +0530

    cursor modified 2 different cursor for touch and no touch screen

--- src/components/Cursor/Cursor.js
+++ src/components/Cursor/Cursor.js
@@ -1,45 +1,91 @@
-import React, {useEffect, useRef} from 'react'
+import React, {useEffect, useState,  useRef} from 'react'
 import './Cursor.css'
 
 function Cursor({isMousein}) {
-    
+  const [isTouchScreen, setIsTouchScreen] = useState(false)
+  const [isTouch, setIsTouch] = useState(false)
   const cursor = useRef()
   const cursorPoint = useRef()
+  const mobileCursor = useRef()
 
   useEffect(() => {
     
     let scrollHeight = window.scrollY
     let cursorY = 0
- 
-    window.addEventListener("mousemove", (e)=>{
+
+    window.addEventListener('touchstart', ()=>{
+      setIsTouchScreen(true)
+      setIsTouch(true)
+    })
+    window.addEventListener('touchend', ()=>{
+      setIsTouch(false)
+    })
+    
+    function funMousemove (e) {
       cursorY = e.clientY
       cursor.current.style.top = e.pageY + 'px'
       cursor.current.style.left = e.clientX + 'px'
       cursorPoint.current.style.top = e.pageY + 'px'
       cursorPoint.current.style.left = e.clientX + 'px'
-    })
-    window.addEventListener("scroll", () => {
+    }
+    function funOnScroll(){
       scrollHeight = window.scrollY;
       cursor.current.style.top = cursorY + scrollHeight+ 'px'
       cursorPoint.current.style.top = cursorY + scrollHeight+'px'
-    });
+    }
+
+    function funTouchmove(e){
+      mobileCursor.current.style.top = e.touches[0].pageY + 'px'
+      mobileCursor.current.style.left = e.touches[0].clientX + 'px'
+    }
+    function funTouchstart(e){
+      mobileCursor.current.style.top = e.touches[0].pageY + 'px'
+      mobileCursor.current.style.left = e.touches[0].clientX + 'px'
+    }
+
+    // for touch devices
+    window.addEventListener('touchstart', funTouchstart)
+    window.addEventListener('touchmove', funTouchmove)
 
     // cursor animation on link hover 
-    if(isMousein){
+    if(!isTouchScreen){
+      // for no touch devices
+      window.addEventListener("mousemove", funMousemove)
+      window.addEventListener("scroll", funOnScroll);
+      if(isMousein){
         cursor.current.classList.add('link-grow')
         cursorPoint.current.classList.add('cp-toggle')
-    } else{
-      cursor.current.classList.remove('link-grow')
-      cursorPoint.current.classList.remove('cp-toggle')
+      } else{
+        cursor.current.classList.remove('link-grow')
+        cursorPoint.current.classList.remove('cp-toggle')
+      }
     }
-  })
+    return(() => {
+      window.removeEventListener("mousemove", funMousemove)
+      window.removeEventListener("scroll", funOnScroll)
+      window.removeEventListener('touchstart', funTouchstart)
+      window.removeEventListener('touchmove', funTouchmove)
+    })
+  }, [isTouch, isTouchScreen, isMousein])
   
-  return (
-    <>
+  // Render cursor acc. to device
+  if (isTouchScreen){
+    if(isTouch){
+      return(
+        <>
+          <div ref={mobileCursor} className="mobileCursor"></div>
+        </>
+      )
+    }
+  } else{
+    return(
+      <>
         <div ref={cursor} className="cursor"></div>
         <div ref={cursorPoint} className="cursor-point"></div>
-    </>
-  )
+      </>
+    )
+  }
 }
+
 
 export default Cursor
