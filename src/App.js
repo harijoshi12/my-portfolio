d8f1a5111ac576a01864221a6fd0f705d18ef0a2
harijoshi12 <mr.hariprasadjoshi@gmail.com>
2022-09-23 19:49:06 +0530

    preloader complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, {  useState } from 'react';
-import {Route, Switch} from "react-router-dom"
+import React, { useState } from 'react';
+import { Route, Switch } from "react-router-dom"
 import Layout from './Ui/Layout';
 import './App.css'
+import './PreLoader.css'
 
 import HomePage from './pages/home/HomePage'
 import AboutPage from './pages/about/AboutPage'
@@ -11,18 +12,26 @@ import SocialLinks from './components/SocialLinks/SocialLinks';
 
 function App() {
   const [isMousein, setIsMousein] = useState(false)
-  
+  const [loading, setLoading] = useState(true)
+  const loader = document.getElementById("preLoader_page")
+
+  if (loader) {
+    setTimeout(() => {
+      loader.style.display = "none"
+      setLoading(false)
+    }, 6000);
+  }
   return (
     <>
-      <Switch>
+      {!loading && (<Switch>
         <Route exact path="/download-resume" component={DownloadResume}></Route>
         <Layout isMousein={isMousein} setIsMousein={setIsMousein} >
-            <Route exact path="/" component={HomePage}></Route>
-            <Route exact path="/about" component={AboutPage}></Route>
-            <Route exact path="/my-resume" component={MyResumePage}></Route>
-          <SocialLinks/>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route exact path="/about" component={AboutPage}></Route>
+          <Route exact path="/my-resume" component={MyResumePage}></Route>
+          <SocialLinks />
         </Layout>
-      </Switch>
+      </Switch>)}
     </>
   );
 }
