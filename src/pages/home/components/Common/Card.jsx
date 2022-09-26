ec1407b3bbd975887521be01e47075314cef05b6
harijoshi12 <mr.hariprasadjoshi@gmail.com>
2022-09-26 23:38:16 +0530

    my work/portfolio section complete

--- src/pages/home/components/Common/Card.jsx
+++ src/pages/home/components/Common/Card.jsx
@@ -3,26 +3,37 @@ import styles from "../../HomePage.module.css";
 import VanillaTilt from "vanilla-tilt";
 
 export const PortfolioItem = (props) => {
-  const { bg, title, desc, features, git_link, live_demo_link } = props;
+  const { id, title, desc, git_link, live_demo_link } = props;
+  const tilt = useRef(null);
   // const bgh= {
   //   bgGradient: 'background-color: #4158D0',
   //   backgroundImage: 'linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)'
   // }
+  useEffect(() => {
+    VanillaTilt.init(tilt.current, {
+      max: 35,
+      speed: 300,
+      glare: true,
+      "max-glare": 0.7,
+    });
+  }, []);
   return (
-    <div className={`${styles.card}  ${styles.portfolio_item}`} style={bg}>
+    <div ref={tilt} className={`${styles.card}  ${styles.portfolio_item}`}>
+      {/* <div className={`${styles.card}  ${styles.portfolio_item}`} style={bg}> */}
       {/* <div className={styles.imgbox} style={{backgroundImage:`url(${img})`}}></div> */}
       {/* <div className={styles.imgbox} style={bg}></div> */}
       <div className={styles.imgbox}></div>
-      <h2>{title}</h2>
-      <p>"{desc}"</p>
-      <div>
+      <h2 className={styles.projectId}>0{id}</h2>
+      <h3 className={styles.projectTitle}>{title}</h3>
+      <p className={styles.projectDesc}>"{desc}"</p>
+      {/* <div>
         <h3>Features</h3>
         {features.map((f, idx) => (
           <p key={idx}>
             <span>&#xBB; </span> <span> {f}</span>
           </p>
         ))}
-      </div>
+      </div> */}
       <div className={styles.btn_container}>
         <a href={git_link}>Github</a>
         <a href={live_demo_link}>Live Demo</a>
--- src/pages/home/components/Portfolio/Portfolio.jsx
+++ src/pages/home/components/Portfolio/Portfolio.jsx
@@ -1,78 +1,92 @@
-import React, {forwardRef} from 'react'
-import {PortfolioItem} from '../Common/Card'
-import styles from '../../HomePage.module.css'
-import portfoioItem_img1 from '../../../../media/images/portfolio/crypto.webp'
-import portfoioItem_img2 from '../../../../media/images/portfolio/socialMedia.jpg'
-import portfoioItem_img3 from '../../../../media/images/portfolio/ecom.jpg'
-function Portfolio(props,ref) {
+import React, { forwardRef } from "react";
+import { PortfolioItem } from "../Common/Card";
+import styles from "../../HomePage.module.css";
+import portfoioItem_img1 from "../../../../media/images/portfolio/crypto.webp";
+import portfoioItem_img2 from "../../../../media/images/portfolio/socialMedia.jpg";
+import portfoioItem_img3 from "../../../../media/images/portfolio/ecom.jpg";
+function Portfolio(props, ref) {
   const portfolioData = [
     {
-      id:1,
-      img:portfoioItem_img1, 
+      id: 1,
+      img: portfoioItem_img1,
       bg: {
-        backgroundColor: '#21D4FD',
-        backgroundImage: 'linear-gradient(19deg, #21D4FD 0%, #B721FF 100%)'
+        backgroundColor: "#21D4FD",
+        backgroundImage: "linear-gradient(19deg, #21D4FD 0%, #B721FF 100%)",
       },
-      title:"My Porfolio Website",
-      desc:  "A website to showcase my skills & Experience", 
-      features: ['Technologies:- React, Firebase', 'Fully Responsive', 'Custom Animated Cursor', 'SVG animations'],
-      git_link:"https://www.harijoshi.tech/", 
-      live_demo_link:"https://www.harijoshi.tech/"
+      title: "My Porfolio Website",
+      desc: "A website to showcase my skills & Experience",
+      features: [
+        "Technologies:- React, Firebase",
+        "Fully Responsive",
+        "Custom Animated Cursor",
+        "SVG animations",
+      ],
+      git_link: "https://www.harijoshi.tech/",
+      live_demo_link: "https://www.harijoshi.tech/",
     },
     {
-      id:2,
-      img:portfoioItem_img2, 
+      id: 2,
+      img: portfoioItem_img2,
       bg: {
-        backgroundColor: '#21D4FD',
-        backgroundImage: 'linear-gradient(19deg, #21D4FD 0%, #B721FF 100%)'
+        backgroundColor: "#21D4FD",
+        backgroundImage: "linear-gradient(19deg, #21D4FD 0%, #B721FF 100%)",
       },
-      title:"Resume Builder", 
+      title: "Resume Builder",
       desc: "A website that lets you build your resume with the template of your choice",
-      features: ['Signup & Login functionality', 'Live Edit functionality', 'Download resume in pfd formate with selectable text and clickable link'], 
-      git_link:"https://github.com/", 
-      live_demo_link:"https://harijoshi.in/"
+      features: [
+        "Signup & Login functionality",
+        "Live Edit functionality",
+        "Download resume in pfd formate with selectable text and clickable link",
+      ],
+      git_link: "https://github.com/",
+      live_demo_link: "https://harijoshi.in/",
     },
     {
-      id:3,
-      img:portfoioItem_img3, 
+      id: 3,
+      img: portfoioItem_img3,
       bg: {
-        backgroundColor: '#21D4FD',
-        backgroundImage: 'linear-gradient(19deg, #21D4FD 0%, #B721FF 100%)'
+        backgroundColor: "#21D4FD",
+        backgroundImage: "linear-gradient(19deg, #21D4FD 0%, #B721FF 100%)",
       },
-      title:"Chat & Video calling app", 
-      desc: "chat and Video call your friends & family for free", 
-      features: ['React Native based Android & ios app', 'Easy to use UI'],
-      git_link:"https://github.com/", 
-      live_demo_link:"https://harijoshi.in/"
+      title: "Chat & Video calling app",
+      desc: "chat and Video call your friends & family for free",
+      features: ["React Native based Android & ios app", "Easy to use UI"],
+      git_link: "https://github.com/",
+      live_demo_link: "https://harijoshi.in/",
     },
     {
-      id:4,
-      img:portfoioItem_img1, 
+      id: 4,
+      img: portfoioItem_img1,
       bg: {
-        backgroundColor: '#21D4FD',
-        backgroundImage: 'linear-gradient(19deg, #21D4FD 0%, #B721FF 100%)'
+        backgroundColor: "#21D4FD",
+        backgroundImage: "linear-gradient(19deg, #21D4FD 0%, #B721FF 100%)",
       },
-      title:"Blockchain based Polling & Voting app", 
-      desc: "app where people can start polling and Voting", 
-      features: ['Technology:- Blockchain, Solidity', "start polling and voiting "],
-      git_link:"https://github.com/", 
-      live_demo_link:"https://harijoshi.in/"
+      title: "Blockchain based Polling & Voting app",
+      desc: "app where people can start polling and Voting",
+      features: [
+        "Technology:- Blockchain, Solidity",
+        "start polling and voiting ",
+      ],
+      git_link: "https://github.com/",
+      live_demo_link: "https://harijoshi.in/",
     },
-  ]
+  ];
   return (
-    <section id='portfolio' ref={ref} className={`${styles.sec} ${styles.portfolio} ${styles.common}`}>
-        <h1 className={styles.title}>My Recent Work</h1>
-       <div className={styles.content_wrapper}>
+    <section
+      id="portfolio"
+      ref={ref}
+      className={`${styles.sec} ${styles.portfolio} ${styles.common}`}
+    >
+      <h1 className={styles.title}>My Recent Work</h1>
+      <div className={styles.content_wrapper}>
         <div className={styles.content}>
-          {portfolioData.map(data=>{
-            return(
-              <PortfolioItem key={data.id} {...data} />
-              )
-            })}
-          </div>
-       </div>
+          {portfolioData.map((data) => {
+            return <PortfolioItem key={data.id} {...data} />;
+          })}
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default forwardRef(Portfolio)
+export default forwardRef(Portfolio);
